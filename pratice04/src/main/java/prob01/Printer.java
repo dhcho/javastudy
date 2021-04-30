b8180eda4d71c47b754c550bfbf1c670bb89f52c
BIT_R41 <BIT_R41@DESKTOP-PA04318>
2021-04-30 17:26:53 +0900

    pratice04 문제 풀이 및 실습

---
+++ pratice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,23 @@
+package prob01;
+
+public class Printer {
+//	public void println(String prt) {
+//		System.out.println(prt);
+//	}
+//	
+//	public void println(int prt) {
+//		System.out.println(prt);
+//	}
+//	
+//	public void println(double prt) {
+//		System.out.println(prt);
+//	}
+//	
+//	public void println(boolean prt) {
+//		System.out.println(prt);
+//	}
+	
+	public <T> void println(T i) {
+		System.out.println(i);
+	}
+}
